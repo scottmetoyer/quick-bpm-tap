0ee6753026f50a761796852e9bada4a79fc287e7
Scott Metoyer <scott.metoyer@gmail.com>
2014-11-07 15:41:04 -0800

    Added a stubbed notification alert

--- www/js/index.js
+++ www/js/index.js
@@ -72,6 +72,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
+        navigator.notification.alert('Welcome to Quick BPM Tap', function () { }, 'Quick BPM Tap', 'OK');
     },
 
     onPointerUp: function (evt) {
