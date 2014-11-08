c8652fe8034c51f3b93c368c8d9f847954e82e2a
Scott Metoyer <scott.metoyer@gmail.com>
2014-11-07 16:01:19 -0800

    Added instruction text to the top of the screen

--- www/js/index.js
+++ www/js/index.js
@@ -72,7 +72,6 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
-        navigator.notification.alert('Welcome to Quick BPM Tap', function () { }, 'Quick BPM Tap', 'OK');
     },
 
     onPointerUp: function (evt) {
